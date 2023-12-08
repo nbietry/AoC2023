b152a574e913ed2fc5342bd935e3ca16a4802a64
nbietry <nbietry@gmail.com>
2023-12-08 18:20:29 +0100

    Day 8 - part 2 solution

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -1,40 +1,61 @@
-import nico.*
-import java.io.DataInput
 import java.io.File
 
 fun main() {
 
-    val lines = File("inputs/inputDay8.txt").readText()
-    val day8 = Day8(lines)
-    println("Part1: " + day8.partOne())
-    println("Part2: " + day8.partTwo())
+    val linesPart1 = File("inputs/inputDay8_part1.txt").readText()
+    val linesPart2 = File("inputs/inputDay8_part2.txt").readText()
+    println("Part1: " + Day8(linesPart1).partOne())
+    println("Part2: " + Day8(linesPart2).partTwo())
 }
 
 class Day8(input: String){
     companion object{
         private val networkRegex = """(\w+) = \((\w+), (\w+)\)""".toRegex()
+        private fun gcd(a: Long, b: Long): Long {
+            return if (b == 0L) a else gcd(b, a % b)
+        }
+
+        private fun lcm(a: Long, b: Long): Long {
+            return a * b / gcd(a, b)
+        }
     }
     private val networkNodes = networkRegex.findAll(input).associateBy(
         keySelector = {it.groupValues[1]},
         valueTransform = {it.groupValues[2] to it.groupValues[3]}
     )
     private val instructions = input.split('\n').first()
+    private val getStartingNodes = networkNodes.filter { it.key.toCharArray().last() == 'A' }
+    private fun executeInstructions(startNode:String) = instructions.fold(startNode) { acc, instruction ->
+        when (instruction) {
+            'L' -> networkNodes[acc]!!.first
+            'R' -> networkNodes[acc]!!.second
+            else -> throw IllegalArgumentException()
+        }
+    }
     fun partOne(): String {
+        //First algo:
         var counter = 0
         var startNode = "AAA"
         do {
-            startNode = instructions.fold(startNode) { acc, instruction ->
-                when (instruction) {
-                    'L' -> networkNodes[acc]!!.first
-                    'R' -> networkNodes[acc]!!.second
-                    else -> throw IllegalArgumentException()
-                }
-            }
+            startNode = executeInstructions(startNode)
             counter++
         }while (startNode != "ZZZ")
+
+        //New algo with discovery on Part 2:
+        val withPart2AlgoResult = generateSequence("AAA", ::executeInstructions).indexOf("ZZZ") * instructions.count()
+        println("Part 1 (with new Algo): $withPart2AlgoResult")
+
         return (counter * instructions.count()).toString()
     }
     fun partTwo(): String {
-        return ""
+
+        val startNodes = getStartingNodes.map { it.key }
+        val result = startNodes.asSequence().map { node ->
+            generateSequence(node, ::executeInstructions).withIndex().first { (index, end) -> end.endsWith('Z') }
+        }.toList().map { it.index.toLong() }
+
+        //Get the lowest common denominator and multiply by instruction size
+        return (result.reduce{acc, i -> lcm(acc, i)} * instructions.count()).toString()
+
     }
 }
