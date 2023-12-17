9e0c8ec8042bb770136e5aee2d0e37624b5d70ed
nbietry <nicolas.bierty@loccitane.com>
2023-12-17 22:23:45 +0100

    Day 15 - Solution part 2

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -3,7 +3,7 @@ import java.io.File
 fun main() {
 
     val linesPart1 = File("inputs/inputDay15.txt").readText()
-    val linesPart2 = File("inputs/inputDay15_example.txt").readText()
+    val linesPart2 = File("inputs/inputDay15.txt").readText()
     println("Part1: " + Day15(linesPart1).partOne())
     println("Part2: " + Day15(linesPart2).partTwo())
 }
@@ -13,7 +13,19 @@ class Day15(input: String) {
     private var sequence = input.split(',')
     fun partOne(): Int = sequence.sumOf { it.hash() }
     fun partTwo(): Int {
-        return 0
+        val facility = List(256) { mutableMapOf<String, Int>() }
+
+        for(step in sequence){
+            val (label, focalLength) = step.split('-','=')
+            if(step.indexOf('=') > 0)
+                facility[label.hash()][label] = focalLength.toInt()
+            else
+                facility[label.hash()].remove(label)
+        }
+
+        return facility.withIndex().sumOf { (index, box) ->
+            (index + 1) * box.values.withIndex().sumOf { (slot, focalLength) -> (slot + 1) * focalLength }
+        }
     }
 
     companion object{
