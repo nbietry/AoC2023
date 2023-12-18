1fb960e4515d6e579946128dcf5e2f6926286d8d
nbietry <nicolas.bierty@loccitane.com>
2023-12-19 00:26:02 +0100

    Day 16 - part 2 solution

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,22 +1,26 @@
+
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import nico.CardinalFacing
 import nico.CardinalFacing.*
 import nico.Vec2
 import java.io.File
 
 fun main() {
-
     val linesPart1 = File("inputs/inputDay16.txt").readText()
-    val linesPart2 = File("inputs/inputDay16_example.txt").readText()
+    val linesPart2 = File("inputs/inputDay16.txt").readText()
     println("Part1: " + Day16(linesPart1).partOne())
-    println("Part2: " + Day16(linesPart2).partTwo())
+    runBlocking {
+        println("Part2: " + Day16(linesPart2).partTwo())
+    }
 }
 
 class Day16(input: String) {
     private val contraption = input.lines()
-    private val visitedNode = mutableListOf<Beam>()
 
     data class Beam(val position: Vec2, val direction: CardinalFacing)
-
     companion object {
         private val directionsMap = mapOf(
             NORTH to '/' to listOf(EAST),
@@ -44,7 +48,6 @@ class Day16(input: String) {
             }
         }
     }
-
     private fun getNextPosition(beam: Beam): List<Beam> {
         val nextPositions =
             directionsMap[beam.direction to contraption[beam.position.y][beam.position.x]] ?: listOf(beam.direction)
@@ -52,10 +55,10 @@ class Day16(input: String) {
         return nextPositions.map { Beam(beam.position.plus(it), it) }
             .filter { it.position.x in contraption[0].indices && it.position.y in contraption.indices }
     }
-
-    fun partOne(): Int {
+    private fun countEnergized(startBeam: Beam): Int {
         val beamList = ArrayDeque<Beam>()
-        val startBeam = Beam(Vec2(0, 0), EAST)
+        val visitedNode = mutableListOf<Beam>()
+
         beamList.add(startBeam)
         visitedNode.add(startBeam)
 
@@ -69,11 +72,28 @@ class Day16(input: String) {
             }
         }
         //renderContraptionWithVisited(contraption, visitedNode.map { it.position })
-
         return visitedNode.map { it.position }.toSet().count()
     }
-
-    fun partTwo(): Int {
-        return 0
+    fun partOne(): Int {
+        val startBeam = Beam(Vec2(0, 0), EAST)
+        return countEnergized(startBeam)
     }
+    suspend fun partTwo(): Int = channelFlow {
+
+        val topSide = (0..<contraption[0].length).map { Vec2(it, 0) }.map { Beam(it, SOUTH) }
+        val bottomSide = (0..<contraption[0].length).map { Vec2(it, contraption.size - 1) }.map { Beam(it, NORTH) }
+        val leftSide = contraption.indices.map { Vec2(0, it) }.map { Beam(it, EAST) }
+        val rightSide = contraption.indices.map { Vec2(contraption[0].length - 1, it) }.map { Beam(it, WEST) }
+
+        for(start in topSide)
+            launch { send(countEnergized(start)) }
+        for(start in bottomSide)
+            launch { send(countEnergized(start)) }
+        for (start in leftSide)
+            launch { send(countEnergized(start)) }
+        for (start in rightSide)
+            launch { send(countEnergized(start)) }
+
+    }.reduce(::maxOf)
+
 }
